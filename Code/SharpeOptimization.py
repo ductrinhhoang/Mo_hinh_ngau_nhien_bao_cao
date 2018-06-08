f29f07a03cc4effc65be3f05d09db21a1e2f4fff
Duc TH <trinhhoangduc.n@gmail.com>
2018-06-08 15:28:45 +0700

    commit

--- Code/SharpeOptimization.py
+++ Code/SharpeOptimization.py
@@ -6,6 +6,7 @@ f = open('config.json')
 json_object = json.load(f)
 muy = json_object["muy"]
 delta = json_object["delta"]
+num_day_get = json_object["num_day_get"]
 
 def Return(Ft, Ft1, r):
     ret = muy * (Ft * r - delta * abs(Ft - Ft1))
@@ -42,10 +43,10 @@ def Sharpe(w, r, N, first_date):
     for i in range(np.size(f)):
         y = np.append(y, i)
 #    plt.gcf().clear()
-    return S
+    return np.sqrt(num_day_get)*S
 
 
-def findW(w, r, N, first_date, alpha, max_loop_count, eps_for_cal_diff):
+def findW(w, r, N, first_date, alpha, max_loop_count, eps_for_cal_diff, eps_for_exit_loop):
     n = np.size(w) - 2
     for k in range(max_loop_count):
         e = np.array([0.0] * (n + 2))
@@ -56,5 +57,9 @@ def findW(w, r, N, first_date, alpha, max_loop_count, eps_for_cal_diff):
             S2 = Sharpe(w - e, r, N, first_date)
             S = (S1 - S2)/(2*eps_for_cal_diff)
             dS = np.append(dS, S)
-        w = w + alpha * dS
+        w1 = w + alpha * dS
+        if(abs(Sharpe(w1 , r, N, first_date) - Sharpe(w , r, N, first_date)) < eps_for_exit_loop):
+            w = w1
+            break
+        w = w1
     return w
--- Code/TT.py
+++ Code/TT.py
@@ -13,11 +13,13 @@ import xlrd
 import json
 import time as TIME
 
+
 def get_price(sheet, num_day_get):
     price = []
     for i in range(1, num_day_get):
         price.append(sheet.cell_value(i, 4))
     return np.array(price)
+
 
 def main():
     start_time = TIME.time()
@@ -33,6 +35,7 @@ def main():
     m = json_object["num_day_for_trade"]  # number of days for trading
     num_day_get = json_object["num_day_get"]
     eps_for_cal_diff = json_object["eps_for_cal_diff"]
+    eps_for_exit_loop = json_object["eps_for_exit_loop"]
     # paremeter
 
     # Get data from excel file
@@ -58,7 +61,8 @@ def main():
     count = N + 1
     while count < (size - 1):
         if (count - N) % m == 1:
-            w = SO.findW(w, r, N, count - N - 1, alpha, max_loop_count, eps_for_cal_diff)
+            w = SO.findW(w, r, N, count - N - 1, alpha,
+                         max_loop_count, eps_for_cal_diff, eps_for_exit_loop)
         state = np.array([1.0])  # state = [1]
         for i in range(n):
             state = np.append(state, r[count - i])
@@ -70,12 +74,11 @@ def main():
         count += 1
 
     gain = np.cumsum(RET)
-    time = np.array([])
-    for i in range(np.size(gain)):
-        time = np.append(time, i)
+    print(gain)
+    time_series = np.array(range(np.size(gain)))
 
     print("Thời gian chạy: ", TIME.time() - start_time)
-    plt.plot(time, gain)
+    plt.plot(time_series, gain)
     plt.show()
 
 
