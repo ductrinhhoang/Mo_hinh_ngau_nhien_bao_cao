e4b593a6b9a5e4ed7b763133b23c636fc6d9e829
Duc TH <trinhhoangduc.n@gmail.com>
2018-06-10 08:23:09 +0700

    [fix] TT

--- Code/TT.py
+++ Code/TT.py
@@ -20,6 +20,7 @@ def get_price(sheet, num_day_get):
         price.append(sheet.cell_value(i, 4))
     return np.array(price)
 
+
 def get_shapre(RET):
     shapre = np.array([0])
     for i in range(1, len(RET)):
@@ -41,7 +42,7 @@ def main():
     n = json_object["num_day_in_state"]  # number of days in state
     N = json_object["num_day_for_train"]  # number of days for training
     m = json_object["num_day_for_trade"]  # number of days for trading
-    num_day_get = json_object["num_day_get"]
+    T = json_object["num_day_get"]
     eps_for_cal_diff = json_object["eps_for_cal_diff"]
     eps_for_exit_loop = json_object["eps_for_exit_loop"]
     # paremeter
@@ -50,36 +51,33 @@ def main():
     wb = xlrd.open_workbook(file_location)
     sheet = wb.sheet_by_index(0)
 
-    price = get_price(sheet, num_day_get)
+    price = get_price(sheet, T+1)
 
-    size = np.size(price)
-    # y = np.array([])
-    # for i in range(size):
-    #     y = np.append(y, i)
-    # plt.plot(y, price)
-    # plt.show()
+    # y = np.array([])7676y
 
-    r = price[1:size] - price[0:size-1]
+    r = price[1:T] - price[0:T-1]
 
     w = np.array([0] * (n + 2))
 
     # print(SO.Sharpe(SO.findW(w, r, N, 0), r, N, 0))
-    RET = np.array([])
+    RET = np.array([0])
     F = 0.0  # Ft time -1
     F_array = np.array([])
     count = N + 1
-    while count < (size - 1):
-        if (count - N) % m == 1:
+    while count < T:
+        if (count - N) % m == 1 or m == 1:
             w = SO.findW(w, r, N, count - N - 1, alpha,
                          max_loop_count, eps_for_cal_diff, eps_for_exit_loop)
         state = np.array([1.0])  # state = [1]
         for i in range(n):
-            state = np.append(state, r[count - i])
+            state = np.append(state, r[count - i - 1])
         state = np.append(state, F)
         Ft = math.tanh(w.dot(state))
         F_array = np.append(F_array, Ft)
-        ret = SO.Return(F, Ft, r[count])
-        RET = np.append(RET, ret)
+        # print(len(F_array), count - 1)
+        if count < T - 1: # caculate ret at tomorrow
+            ret = SO.Return(F, Ft, r[count])
+            RET = np.append(RET, ret)
         F = Ft
         count += 1
 
@@ -87,19 +85,22 @@ def main():
     time_series = np.array(range(np.size(gain)))
 
     print("Thời gian chạy: ", TIME.time() - start_time)
+
+    plt.subplot(3, 1, 1)
     plt.title("PnL")
     plt.plot(time_series, gain)
-    plt.show()
 
+    plt.subplot(3, 1, 2)
     time_series = np.array(range(np.size(F_array)))
     plt.title("Long-Short")
     plt.plot(time_series, F_array)
-    plt.show()
 
+    plt.subplot(3, 1, 3)
     plt.title("Sharpe")
     real_shapre = get_shapre(RET)
     time_series = np.array(range(np.size(real_shapre)))
     plt.plot(time_series, real_shapre)
+
     plt.show()
 
 
